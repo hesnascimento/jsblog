a149163f9c39011d53a40285d187f10c28c203b7
Henrique Souza Nascimento <henrique@grupocs.com.br>
2020-03-28 21:54:22 -0300

    FEAT: Confirm accont service

--- src/Services/Users.js
+++ src/Services/Users.js
@@ -5,10 +5,10 @@ const crypto = require('crypto')
 
 const register = async user => {
   if (!user)
-    throw new ServiceError('400', 'User not sent!')
+    throw new ServiceError(400, 'User not sent!')
 
   if (!UserHelpers.isUserValid(user))
-    throw new ServiceError('400', 'Invalid user!')
+    throw new ServiceError(400, 'Invalid user!')
 
   const { SALT, CONFIRMATION_CODE_LENGTH } = process.env
 
@@ -26,12 +26,63 @@ const register = async user => {
     creationDate: new Date()
   }
 
-  await Users.create(userToDb)
+  try {
+    await Users.create(userToDb)
 
-  // TODO: Send mail
-  return true
+    // TODO: Send mail
+    return true
+  } catch (error) {
+    console.error(
+      'REQUEST =>',
+      user,
+      'ERROR =>',
+      error)
+
+      throw new ServiceError(500, 'Internal server error.')
+  }
+}
+
+const confirmAccount = async code => {
+  if (!code)
+    throw new ServiceError(400, 'Confirmation code not sent!')
+
+  // TODO: Validate confirmation code format
+
+  try {
+    const user = await Users.findOne({
+      where: {
+        confirmed: false,
+        confirmationCode: code
+      }
+    })
+
+    if (!user)
+      throw new ServiceError(404, 'Not found.')
+
+    await Users.update({
+      confirmed: true,
+      confirmationCode: null,
+      confirmationDate: new Date()
+    }, {
+      where: {
+        id: user.id
+      }
+    })
+
+    // Send wellcome mail
+    return true
+  } catch (error) {
+    console.error(
+      'REQUEST =>',
+      code,
+      'ERROR =>',
+      error)
+
+      throw new ServiceError(500, 'Internal server error.')
+  }
 }
 
 module.exports = {
-  register
+  register,
+  confirmAccount
 }
